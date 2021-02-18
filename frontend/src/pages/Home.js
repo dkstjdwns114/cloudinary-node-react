f3324c2d359f95cccd361f1facce771b5a945ccd
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-18 19:27:22 +0900

    완료

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from "react";
+import { Image } from "cloudinary-react";
 
 export default function Home() {
+  const [imageIds, setImageIds] = useState();
+
+  const loadImages = async () => {
+    try {
+      const res = await fetch("/api/images");
+      const data = await res.json();
+      console.log(data);
+      setImageIds(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    loadImages();
+  }, []);
   return (
     <div>
       <h1 className="title">Home</h1>
+      {imageIds &&
+        imageIds.map((imageId, idx) => {
+          return (
+            <Image
+              key={idx}
+              cloudName="anstagram123"
+              publicId={imageId}
+              width="300"
+              crop="scale"
+            />
+          );
+        })}
     </div>
   );
 }
