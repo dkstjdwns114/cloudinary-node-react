7f3b7094fd04159be60ec940736f27636976bb47
dkstjdwns114 <sungjoon256@gmail.com>
2021-02-18 16:33:08 +0900

    SETUP

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="title">Home</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Upload.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+export default function Upload() {
+  return (
+    <div>
+      <h1 className="title">Upload</h1>
+    </div>
+  );
+}
